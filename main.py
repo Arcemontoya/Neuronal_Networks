657da1b7c007096635c73a77df9f39eda2956278
Cota <brandon.cota@uabc.edu.mx>
2024-05-24 11:24:33 -0700

    Se agregaron graficas para presicion y peprdida

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Loss import *
 import nnfs
 from Optimizer import *
 from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
 
 X, y = spiral_data(samples=100, classes=3)
 
@@ -20,7 +21,10 @@ optimizer = Optimizer_Adam(learning_rate=0.02, decay=5e-7)
 #optimizer = Adadelta_Optimizer(learning_rate=1, decay=0.9, epsilon=1e-7) # Corregir
 #optimizer = GDX_Optimizer(initial_learning_rate=0.01) # Corregir
 
-for epoch in range(10001):
+accuracy_list = []
+loss_list = []
+
+for epoch in range(1000):
 
     dense1.forward(X)
     activation1.forward(dense1.output)
@@ -31,11 +35,13 @@ for epoch in range(10001):
     regularization_loss = loss_activation.loss.regularization_loss(dense1) + loss_activation.loss.regularization_loss(dense2)
 
     loss = data_loss + regularization_loss
+    loss_list.append(loss)
 
     predictions = np.argmax(loss_activation.output, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions==y)
+    accuracy_list.append(accuracy)
 
     if not epoch%100:
         print(f'epoch: {epoch}, ' +
@@ -71,3 +77,15 @@ for epoch in range(10001):
 #accuracy = np.mean(predictions == y_test)
 
 #print(f'validation, acc: {accuracy: .3f}, loss: {loss:.3f}')
+
+plt.plot(accuracy_list)
+plt.title("Precision")
+plt.xlabel("Iteraciones")
+plt.ylabel("Valor")
+plt.show()
+
+plt.plot(loss_list)
+plt.title("Perdida")
+plt.xlabel("Iteraciones")
+plt.ylabel("Valor")
+plt.show()
